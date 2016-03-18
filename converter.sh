681f2ac2635c9192ad7eaa3a484a4a336635c993
KunishimaTakeo <kunishimatakeo@ims-lab-macosx-server.local>
2016-03-18 15:35:53 +0900

    add options for converting separately.

--- converter.sh
+++ converter.sh
@@ -1,19 +1,70 @@
 #!/bin/sh
 
-INFILE=$1
-BASE=$2
-OUTDIR=$3
+PROGNAME=$(basename $0)
+
+usage() {
+  echo "Usage: $PROGNAME [-m] [-w] [-i] INFILE BASE OUTDIR"
+  echo "Options:"
+  echo "  -h: show this help"
+  echo "  -m: encode H.264"
+  echo "  -w: encode WebM"
+  echo "  -i: generate screenshots (both Jpeg and PNG)"
+  echo
+  exit 1
+}
+
+for OPT in "$@"; do
+  case "$OPT" in
+  '-h' )
+    usage
+    exit 1
+    ;;
+  '-m' )
+    ENC_H264=1
+    shift
+    ;;
+  '-w' )
+    ENC_WEBM=1
+    shift
+    ;;
+  '-i' )
+    ENC_IMG=1
+    shift
+    ;;
+  *)
+    param=("$@")
+    ;;
+  esac
+done
+
+if [ "$ENC_H264" != 1 -a "$ENC_WEBM" != 1 -a "$ENC_IMG" != 1 ]; then
+  ENC_H264=1
+  ENC_WEBM=1
+  ENC_IMG=1
+fi
+
+INFILE=${param[0]}
+BASE=${param[1]}
+OUTDIR=${param[2]}
 
 mkdir -p ${OUTDIR}
 
-ffmpeg -y -i ${INFILE} \
-  -c:v libx264 -crf 22 -profile:v baseline \
-  -pix_fmt yuv420p -movflags +faststart \
-  ${OUTDIR}/${BASE}.mp4
-ffmpeg -y -i ${INFILE} \
-  -c:v libvpx-vp9 -crf 22 \
-  -deadline realtime -cpu-used -5 \
-  -c:a libvorbis -q:a 4 \
-  ${OUTDIR}/${BASE}.webm
-ffmpeg -y -i ${INFILE} -vframes 1 ${OUTDIR}/${BASE}.jpg
-ffmpeg -y -i ${INFILE} -vframes 1 ${OUTDIR}/${BASE}.png
+if [ "$ENC_H264" = 1 ]; then
+  ffmpeg -y -i ${INFILE} \
+    -c:v libx264 -crf 22 -profile:v baseline \
+    -pix_fmt yuv420p -movflags +faststart \
+    ${OUTDIR}/${BASE}.mp4
+fi
+
+if [ "$ENC_WEBM" = 1 ]; then
+  ffmpeg -y -i ${INFILE} \
+    -c:v libvpx-vp9 -crf 22 \
+    -deadline realtime -cpu-used -5 \
+    -c:a libvorbis -q:a 4 \
+    ${OUTDIR}/${BASE}.webm
+fi
+
+if [ "$ENC_IMG" = 1 ]; then
+  ffmpeg -y -i ${INFILE} -vframes 1 ${OUTDIR}/${BASE}.jpg
+  ffmpeg -y -i ${INFILE} -vframes 1 ${OUTDIR}/${BASE}.png
+fi
