8c55dca0cd4c92be8928e2518abbab8d080d97ae
KunishimaTakeo <kunishimatakeo@ims-lab-macosx-server.local>
2016-03-18 15:17:12 +0900

    more improvement for conversion options of H.264 and WebM.

--- converter.sh
+++ converter.sh
@@ -6,8 +6,14 @@ OUTDIR=$3
 
 mkdir -p ${OUTDIR}
 
-ffmpeg -y -i ${INFILE} -vcodec libx264 -pix_fmt yuv420p -profile:v baseline \
-  -preset slow -crf 22 -movflags +faststart ${OUTDIR}/${BASE}.mp4
-ffmpeg -y -i ${INFILE} -c:v libvpx -qmin 10 -qmax 42 -c:a libvorbis -q:a 4 ${OUTDIR}/${BASE}.webm
+ffmpeg -y -i ${INFILE} \
+  -c:v libx264 -crf 22 -profile:v baseline \
+  -pix_fmt yuv420p -movflags +faststart \
+  ${OUTDIR}/${BASE}.mp4
+ffmpeg -y -i ${INFILE} \
+  -c:v libvpx-vp9 -crf 22 \
+  -deadline realtime -cpu-used -5 \
+  -c:a libvorbis -q:a 4 \
+  ${OUTDIR}/${BASE}.webm
 ffmpeg -y -i ${INFILE} -vframes 1 ${OUTDIR}/${BASE}.jpg
 ffmpeg -y -i ${INFILE} -vframes 1 ${OUTDIR}/${BASE}.png
